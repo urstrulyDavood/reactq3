467347c5c998b1dad9f62e7e55a07635f8ae7852
school <school@QISCET.EDU.IN>
2023-04-03 16:32:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import Display from './Dispaly';
+
+
+
+function App() {
+  return (
+   
+    <div className="App">
+     <Display></Display>
+    </div>
+   
+  );
+}
+
+export default App;
